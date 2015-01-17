2d29db190abeea3416cc6c4752b73c81b60389a5
Hackbright Student <info@hackbrightacademy.com>
2015-01-16 16:57:09 -0800

    updated greatest & largest

--- skills01.py
+++ skills01.py
@@ -1,7 +1,6 @@
 # Things you should be able to do.
 
-number_list = [3, 2, 7, 2]
-#num_list = [ 6, 1, 4, 8, 15, 160, 23, 42, 2, 7]
+number_list = [3, 2, 77, 2]
 word_list = [ "What", "about", "the", "Spam", "sausage", "spam", "spam", "bacon", "spam", "tomato", "and", "spam"]
 
 # Write a function that takes a list of numbers and returns a new list with only the odd numbers.
@@ -39,9 +38,9 @@ print smallest(number_list)
 # Write a function that finds the largest element in a list of integers and returns it.
 def largest(number_list):
     greatest_so_far = number_list[0]
-    for number in number_list:
-        if number > greatest_so_far:
-            greatest_so_far = number
+    for number in range(1, len(number_list)):
+        if number_list[number] > greatest_so_far:
+            greatest_so_far = number_list[number]
     return greatest_so_far
 
 print largest(number_list)
@@ -63,7 +62,7 @@ print word_lengths(word_list)
 # Write a function (using iteration) that sums all the numbers in a list.
 def sum_numbers(number_list):
     sum_num = 0
-    for i in range(0,len(number_list)):
+    for i in range(len(number_list)):
         sum_num = sum_num + number_list[i]
     return sum_num
 
